0418f634685fb66a48e87915189e3d85cd92d907
NaanTeam <naanteam@gmail.com>
2013-10-01 14:51:08 -0700

    First time commit

---
+++ main.c
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.c
+ * Author: Connor
+ *
+ * Created on September 29, 2013, 11:52 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <plib.h>
+void SetupTimer1();
+
+int main(int argc, char** argv)
+{
+    TRISFbits.TRISF0 = 0;
+    
+    SetupTimer1();
+    INTEnableInterrupts();
+
+    while (1)
+    {
+    }
+    return (EXIT_SUCCESS);
+}
+
+void SetupTimer1()
+{
+    INTClearFlag(INT_T1);
+    INTSetVectorPriority(INT_TIMER_1_VECTOR, INT_PRIORITY_LEVEL_3);
+    INTSetVectorSubPriority(INT_TIMER_1_VECTOR, INT_SUB_PRIORITY_LEVEL_0);
+    INTEnable(INT_T1, INT_ENABLED);
+    
+    
+    // Enable multi-vector interrupts
+    INTConfigureSystem(INT_SYSTEM_CONFIG_MULT_VECTOR);
+    //Enable
+    INTEnableInterrupts();
+
+    OpenTimer1(T1_ON | T1_SOURCE_INT | T1_PS_1_8, 0xFFFF);
+}
+
+void __ISR(_TIMER_1_VECTOR, IPL3AUTO) Timer1Handler(void)
+{
+    PORTFbits.RF0 = PORTFbits.RF0 ^ 1;;
+    IFS0CLR = 0x00000010; // Be sure to clear the Timer1 interrupt status
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TimerTesting.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TimerTesting.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=timertesting.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/timertesting.x.tar
+# local_sim configuration
+CND_ARTIFACT_DIR_local_sim=dist/local_sim/production
+CND_ARTIFACT_NAME_local_sim=TimerTesting.X.production.hex
+CND_ARTIFACT_PATH_local_sim=dist/local_sim/production/TimerTesting.X.production.hex
+CND_PACKAGE_DIR_local_sim=${CND_DISTDIR}/local_sim/package
+CND_PACKAGE_NAME_local_sim=timertesting.x.tar
+CND_PACKAGE_PATH_local_sim=${CND_DISTDIR}/local_sim/package/timertesting.x.tar
